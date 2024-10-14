b1586f9f0eee9a9ae045550bb56ee6bbed51df16
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-10-14 12:50:50 -0700

    update package.json

--- public/checker.js
+++
@@ -1,5 +0,0 @@
-let REPL_NAME = 'videos'
-
-if (location.href.split('://')[1].split('.')[0] === REPL_NAME && location.hostname.endsWith('.repl.co/') === true) {
-  location.href = location.href.split('repl.co')[0] + 'com'
-}
